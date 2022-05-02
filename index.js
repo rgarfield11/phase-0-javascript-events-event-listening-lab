a8835137621bf207984253956e773870dad2332c
Ross <rossgarfield@gmail.com>
2022-05-01 19:30:23 -0500

    commit tree 5d61b933ac109db9ecf0fca33296374c0310ad37

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+const clickAlert = () => alert('I was clicked!');
+input.addEventListener('click', clickAlert);
 }
+
+
+// const input = document.getElementById('input');
+// const clickAlert = () => alert('I was clicked!');
+// input.addEventListener('click', clickAlert);
